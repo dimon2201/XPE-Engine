3817266241b0b31c4264b4682d1198caabada580
dmmon01 <dmytrii4real@gmail.com>
2023-07-03 20:22:38 +0300

    Introduce basic ImageManager

--- engine/include/core/core.hpp
+++ engine/include/core/core.hpp
@@ -4,6 +4,7 @@
 #include <core/canvas.hpp>
 #include <core/d3d11_context.hpp>
 #include <core/ecs.hpp>
+#include <core/image_manager.hpp>
 #include <core/memory_pool.hpp>
 #include <core/profiler.hpp>
 #include <core/rendering.hpp>
